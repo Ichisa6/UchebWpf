974a37cbd7520773a69d7c77547e276c780eb39e
Ichisa6 <sublosd69@gmail.com>
2023-04-27 13:31:28 +0300

    first commit

---
+++ UchebVerstka/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UchebVerstka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<StatusUser> list = new List<StatusUser>()
+            {
+            new StatusUser()
+            {
+                Id = 1, LName = "Ivanov", FName = "Ivan", Age = 15, City = "Kazan"
+            },
+            new StatusUser() 
+            { 
+            Id = 1 , LName = "Petrov", FName = "Ivan", Age = 15, City = "Kazan" 
+            }
+            };
+            LVStatus.ItemsSource = list;
+            
+        }
+    }
+}
---
+++ UchebVerstka/StatusUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UchebVerstka
+{
+    internal class StatusUser
+    {
+        public StatusUser()
+        {
+        }
+
+        public StatusUser(int id, string lName, string fName, int age, string city)
+        {
+            Id = id;
+            LName = lName;
+            FName = fName;
+            Age = age;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string LName { get; set; }
+        public string FName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+
+    }
+}
